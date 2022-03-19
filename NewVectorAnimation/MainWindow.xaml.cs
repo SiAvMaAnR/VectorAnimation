4f9b7f14a23da076d2861f4bb565d22b527f687d
SiAvMaAnR <SiAvMaAnR@gmail.com>
2022-03-19 20:29:55 +0300

    xaml commit

--- NewVectorAnimation/MainWindow.xaml.cs
+++ NewVectorAnimation/MainWindow.xaml.cs
@@ -9,13 +9,10 @@ using System.Threading.Tasks;
 
 namespace NewVectorAnimation
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         private MainWindowViewModel mainWindowViewModel;
-        private Storyboard storyboard;
+        //private Storyboard storyboard;
 
 
         public MainWindow()
@@ -24,53 +21,47 @@ namespace NewVectorAnimation
             mainWindowViewModel = new MainWindowViewModel();
             DataContext = mainWindowViewModel;
 
-            storyboard = new Storyboard();
+            //storyboard = new Storyboard();
         }
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            await Dispatcher.BeginInvoke(() =>
-            {
-                DoubleAnimationUsingPath animationX = new DoubleAnimationUsingPath()
-                {
-                    PathGeometry = this.WavePath.Data.GetFlattenedPathGeometry(),
-                    Source = PathAnimationSource.X,
-                    Duration = new Duration(TimeSpan.FromSeconds(20)),
-                    IsAdditive = true,
-                    IsCumulative = true
-                };
+            //await Dispatcher.BeginInvoke(() =>
+            //{
+            //    DoubleAnimationUsingPath animationX = new DoubleAnimationUsingPath()
+            //    {
+            //        PathGeometry = this.WavePath.Data.GetFlattenedPathGeometry(),
+            //        Source = PathAnimationSource.X,
+            //        Duration = new Duration(TimeSpan.FromSeconds(20)),
+            //        IsAdditive = true,
+            //        IsCumulative = true
+            //    };
 
-                DoubleAnimationUsingPath animationY = new DoubleAnimationUsingPath()
-                {
-                    PathGeometry = this.WavePath.Data.GetFlattenedPathGeometry(),
-                    Source = PathAnimationSource.Y,
-                    Duration = new Duration(TimeSpan.FromSeconds(20)),
-                };
+            //    DoubleAnimationUsingPath animationY = new DoubleAnimationUsingPath()
+            //    {
+            //        PathGeometry = this.WavePath.Data.GetFlattenedPathGeometry(),
+            //        Source = PathAnimationSource.Y,
+            //        Duration = new Duration(TimeSpan.FromSeconds(20)),
+            //    };
 
-                DoubleAnimationUsingPath animationAngle = new DoubleAnimationUsingPath()
-                {
-                    PathGeometry = this.WavePath.Data.GetFlattenedPathGeometry(),
-                    Source = PathAnimationSource.Angle,
-                    Duration = new Duration(TimeSpan.FromSeconds(20)),
-                };
+            //    DoubleAnimationUsingPath animationAngle = new DoubleAnimationUsingPath()
+            //    {
+            //        PathGeometry = this.WavePath.Data.GetFlattenedPathGeometry(),
+            //        Source = PathAnimationSource.Angle,
+            //        Duration = new Duration(TimeSpan.FromSeconds(20)),
+            //    };
 
-                Storyboard.SetTargetName(animationX, "ShipPath");
-                Storyboard.SetTargetName(animationY, "ShipPath");
-                Storyboard.SetTargetName(animationAngle, "ShipPath");
+            //    Storyboard.SetTargetName(animationX, "ShipPath");
+            //    Storyboard.SetTargetName(animationY, "ShipPath");
+            //    Storyboard.SetTargetName(animationAngle, "ShipPath");
 
-                Storyboard.SetTargetProperty(animationX, new PropertyPath("(Canvas.Left)"));
-                Storyboard.SetTargetProperty(animationY, new PropertyPath("(Canvas.Top)"));
-                Storyboard.SetTargetProperty(animationAngle, new PropertyPath("(UIElement.RenderTransform).(RotateTransform.Angle)"));
-
-
-                storyboard.Children.Add(animationX);
-                storyboard.Children.Add(animationY);
-                storyboard.Children.Add(animationAngle);
-
-                storyboard.Begin(this, true);
+            //    Storyboard.SetTargetProperty(animationX, new PropertyPath("(Canvas.Left)"));
+            //    Storyboard.SetTargetProperty(animationY, new PropertyPath("(Canvas.Top)"));
+            //    Storyboard.SetTargetProperty(animationAngle, new PropertyPath("(UIElement.RenderTransform).(RotateTransform.Angle)"));
 
 
-            });
+
+            //});
 
         }
     }
